d348f1035bd0524bf22682649a3be78d145a7b77
shivam-yadav-d9 <shivammpcps@gmail.com>
2024-06-23 10:46:11 +0530

    first commit

---
+++ Basic.js
@@ -0,0 +1,144 @@
+
+// let a = 5;
+// document.write(a);         //5
+// document.write(typeof a);  //num 
+
+
+// let b =10;
+// document.write(b);         //1o
+// document.write(typeof b)    //num
+
+
+// let c = true;
+// document.write(c);           //true
+// document.write(typeof c);    //boolean
+
+
+// let d = null;
+// document.write(d);           //null
+// document.write(typeof d);     //object
+
+
+// let e;
+// document.write(e);                  //undefined
+// document.write(typeof e);            //undefined
+
+
+// let f;
+// document.write(f);                         //undefined
+// document.write(typeof f);                 //undefined
+
+
+// let a = "5";
+// document.write(a);                        //5   
+// document.write(typeof a);                  //string
+// document.write(typeof Number(a));           //number
+
+
+// let b =5;
+// document.write(b);                         //5
+// document.write(typeof b);                  //number
+// document.write(typeof String(b));             //string
+
+
+// let name = "shivam";
+// document.write(name);                       //shivam
+// document.write(typeof name);                //string
+// document.write(typeof Number(name));          //Number
+
+
+// let fullName = "shivam yadav";
+// document.write(fullName);                       //shivam yadav
+// document.write(typeof fullName);                  //string
+// document.write(typeof Number(fullName));           //Number
+
+
+// document.write(null>0);           //false
+// document.write(null<0);           //false
+// document.write(null<=0);          //true
+// document.write(null>=0);            //true
+
+
+// let name = "shivam yadav";
+// document.write(name);                 //shivam yadav
+// document.write(typeof name);           //string
+// let result = parseInt(name);            //NaN
+
+
+// let value = "44.5";
+// document.write(value);                         //44.5
+// document.write(typeof value);                 // string    
+
+// let result = parseInt(value);           
+// document.write(result);                     // 44 
+// document.write(typeof result);                 //Number
+
+
+// if(null==null){
+//     document.write("Both are equal");
+
+// }
+// else{                                                 // Both are equal
+//     document.write("Not equal");
+// }
+
+
+// document.write(parseInt(123));      //123
+
+// document.write(parseInt("123"));        //123     //type number
+
+// document.write(parseInt("123","10"));     //123
+
+// document.write(parseInt("077"));                //77
+
+// document.write(parseFloat("44.5."));            //44.5
+
+// let a = "44.5";
+// let res = (parseInt(a));            
+// document.write(res)                              //44
+// document.write(typeof res);                     //number
+
+
+// let a = "44.5";
+// let res = (parsefloat(a));            
+// document.write(res)                              //44.5
+// document.write(typeof res);                     //number
+
+
+
+// document.write(parseInt("$123"))            //NaN
+
+// document.write(parseInt("-345"));           //-345
+
+// document.write(parseInt("XYZ"));             //NaN
+
+// document.write(isNaN("shivam"));                  //true
+
+// document.write(isNaN("5"));                   //false
+
+
+//  TYPE COERICION VERY IMPORTANT FOR INTERVIEW
+
+// let sum = "shivam"-5;
+// document.write(sum);          //NaN
+
+// let add = "shivam"+5;
+// document.write(add);            //shivam5
+
+// let  value = "5"-"5"
+// document.write(value);            //0
+
+// let result = "5"-5;
+// document.write(result);             //0
+
+// let plus = "yadav"+5;
+// document.write(plus);            //yadav5
+
+
+
+// document.write(10+"20");             //1020
+
+// document.write(9-"5");              //4
+
+// document.write(" "+" ");            //  '' 
+
