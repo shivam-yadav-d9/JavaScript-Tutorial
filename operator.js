ad695f6f9fa32a7607b98de740e1061a9c8d0a77
shivam-yadav-d9 <shivammpcps@gmail.com>
2024-06-26 10:17:51 +0530

    first commite

---
+++ operator.js
@@ -0,0 +1,59 @@
+//*      operator:
+
+//*    Arithmatic operator
+
+// var a=5;
+// var b=4;
+// document.write("sum of a+b=",a+b);               //9
+// document.write( "sub of a-b=",a-b);              //1
+// document.write("multiplication of a*b",a*b);      //20
+// document.write("divsion of a/b",a/b);             //1.25
+// document.write("modulo of a%b",a%b);              //1
+// document.write("Exponentiation of a**b",a**b);     //625
+
+
+//* Assiment operator
+
+// let a= 3;
+// let b=4;
+// a*=a;
+// document.write(a);            //9
+// b/=b;
+// document.write(b);             //1
+
+// a**=a;
+// document.write(a);           //27
+
+
+//* Comparison Operator
+
+
+// let a=6;
+// let b=6.0;
+// document.write(a==b);            //true
+// document.write(a>b);                //false
+// document.write(a<b);                  //false
+// document.write(a===b);                //true
+// document.write(a!==b);                   //false
+
+
+
+// *UNARY OPERATOR
+
+// *Increment 
+
+// var a=5;
+// document.write(a++);          //5
+
+// document.write(++a);          //6
+
+// document.write(a)            //5
+
+
+//  *Decrement 
+
+// var a =6;
+// document.write(a--);       //6
+// document.write(a)          //5
+
+// document.write(--a);       //5
