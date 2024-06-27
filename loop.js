8bddf80b256ceb96d9f0407d632cc824ddddafec
shivam-yadav-d9 <shivammpcps@gmail.com>
2024-06-27 12:33:49 +0530

    first commit

---
+++ loop.js
@@ -0,0 +1,96 @@
+// loop statement
+
+// let num = prompt("enter the number");
+// for(let i=1;i<10;i++){
+//     document.write(`${num}*${i}=${num*i}<br>`)
+// }
+
+
+
+// let  table = 5;
+// for(let i=1;i<=10;i++){
+//     document.write(table*i)
+// }
+
+
+
+// for(let i=29;i>=1;i--)
+// {
+//     document.write(i)
+// }
+
+
+
+// for(let i=1;i<=29;i++){
+//     document.write(i)
+// }
+
+
+
+// for(let i=1;i<=50;i++){
+//     document.write("shivam yadav")
+// }
+
+
+
+// let sum = 0 ;
+// for(let i=1;i<=5;i++){
+//     sum = sum+i;
+// }
+// document.write(sum);
+
+
+
+
+// let sum = 0 ;
+// for(let i=5;i>=1;i--){
+//     sum = sum+i;
+// }
+// document.write(sum);
+
+
+
+
+// for(let i=1;i<=10;i++){
+//     if(i==7){
+//         break;
+//     }
+//     document.write(i)
+// }
+
+
+
+// let num = 100;
+// for(let i=1;i<=num;i++){
+//     if(i%2==0){
+//         document.write(i)
+//     }
+    
+// }
+// document.write("Thanku")
+
+
+
+
+// let value = 100;
+// for(let i=1;i<=value;i++){
+//     if(i%2!=0){
+//         document.write(i)
+//     }
+    
+// }
+// document.write("Thanku")
+
+
+
+
+
+// let year=2002;
+// let leap;
+// if(year%4==0&&year%100!=0){
+//    leap=("This is a leap Year");                                       leap Year program 
+// }
+// else{
+//     leap=("Not a leap Year");
+// }
+// document.write(leap); 
