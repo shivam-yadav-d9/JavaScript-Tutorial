141121de84b0edde4188615ed5843b0f6b591a0e
shivam-yadav-d9 <shivammpcps@gmail.com>
2024-07-01 11:28:42 +0530

    first commite

---
+++ conditional.js
@@ -0,0 +1,126 @@
+//conditioal statements
+
+// let age = 12;
+// if(age>=18){
+//     document.write("you can marry !");
+// }
+// else{
+//     document.write("you are still child !")
+// }
+
+
+
+// let mode ="light";
+// if(mode=="light"){
+//     document.write("light mode")
+// }
+// else{
+//     document.write("dark mode")
+// }
+
+
+
+// let mode ="white";
+// if(mode=="dark"){
+//     document.write("black")
+// }
+// else if(mode=="light"){
+//     document.write("lighter")
+// }
+// else if(mode=="green"){
+//     document.write("greener")
+// }
+// else{
+//     document.write("white")
+// };
+
+
+
+// let temp = 23;
+// if(temp==40){
+//     document.write("I will go outside")
+// }
+// else if(temp==30){
+//     document.write("I will do excercise")
+// }
+// else if(temp>=15){
+//     document.write("I will study")
+// }
+// else{
+//     document.write("I will sleep")
+// }
+
+
+
+// let  num = 22;
+// if(num%2==0){
+//     document.write("Even number")
+// }
+// else{
+//     document.write("Odd number")
+// }
+// if(num>0){
+//     document.write("possitive number")
+// }
+// if(num<0){
+//     document.write("Negative  number")
+// }
+
+
+
+// let marks = 70;
+// if(marks>=90 && marks<=100){
+//     document.write("A")
+// }
+// else if(marks>=70 && marks<=90){
+//     document.write("B")
+// }
+// else if(marks>=30 && marks<=70){
+//     document.write("C")
+// }
+// else{
+//     document.write("you fail")
+// }
+
+
+
+// let letter = "G";
+// if(letter>="A" && letter<="Z"){
+//     document.write("Upper case")
+// }
+// else if( letter>="a" && letter<="z"){
+//     document.write("Lower case")
+// }
+// else{
+//     document.write("Not a letter")
+// }
+
+
+
+// let day = 2;
+// switch(day){
+//     case 1:document.write("monday")
+//     break;
+//     case 2:document.write("tuesday")
+//     break;
+//     case 3:document.write("wednesday")
+//     break;
+//     case 4:document.write("thursday")
+//     break;
+//     case 5:document.write("friday")
+//     break;
+//     case 6:document.write("saturday")
+//     break;
+//     default: document.write("sunday")
+// };
+
+
+
+
+// let lenght = prompt("enter your lenght")
+// let width = prompt("enter your width")
+// if(isNaN(lenght) || isNaN(width)){
+//     document.write("not a valid number")
+// }
+// let rectangle = lenght*width;
+// document.write(" rectangle area = ",rectangle);
